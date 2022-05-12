04572929f7fa712944a4978374360792e629c7ac
leonorahofmann <Leonorahofmann76@googlemail.com>
2022-05-12 12:56:32 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let wurf1 = 0
+let wurf2 = 0
+let anzahlWuerfe1
+let anzahlWuerfe2
+let summeSpieler1
+let summeSpieler2
+let spielerAnDerReihe = 1
+
+function wuerfeln (){
+    if (spielerAnDerReihe == 1)
+}
+ let wurf1 = Math.floor(Math.random()*6 + 1)
+let wurf2 = Math.floor(Math.random()*6 + 1)
+
+
